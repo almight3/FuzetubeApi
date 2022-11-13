c9d0d84b36defebc8a3ad604070b7670645e7550
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-13 18:36:52 +0530

    changed token setting

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,17 +1,18 @@
 const sendToken = (userData,statusCode,res)=>{
     const token = userData.generateJWT()
-    const options = {
-        expires: new Date(
-          Date.now() + 1 * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-    };
+    // const options = {
+    //     expires: new Date(
+    //       Date.now() + 1 * 24 * 60 * 60 * 1000
+    //     ),
+    //     httpOnly: true,
+    // };
     const user = {
       _id:userData._id,
       email:userData.email,
       username:userData.username,
+      token:token
     }
-   res.status(statusCode).cookie("fuzetube",token,options).json({
+    res.status(statusCode).json({
     success:true,
     user,
    })
