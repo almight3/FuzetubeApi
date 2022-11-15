3e48dcfb640b15a7f165dc3f9b2854261a807248
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-15 11:43:32 +0530

    fix - verifying null token

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,12 +4,12 @@ import ErrorHandler from "../utils/errorHandler.js";
 import jwt from "jsonwebtoken";
 
 export const authenticateUser = catchAsyncError(async (req,res,next)=>{
-  const authHeader = req.headers.authorization
-  const token = authHeader.split(" ")[1]
-    if (!token) {
+
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(" ")[1]
+      if(token==="null") {
       return next(new ErrorHandler("Please Login to access this resource", 401));
     }
-  
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
   
     req.user = await User.findById(decodedData.id);
